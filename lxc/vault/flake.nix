c89e3ca503865b0f8c7853d83ff8aa7934ee4e32
Fester Herenius <me@festerherenius.nl>
2023-09-02 23:03:33 +0200

    Started on Nix config

---
+++ lxc/vault/flake.nix
@@ -0,0 +1,23 @@
+{
+  description = "";
+  inputs.deploy-rs.url = "github:serokell/deploy-rs";
+
+  outputs = { self, nixpkgs, deploy-rs }: {
+    nixosConfiguration.vault = nixpkgs.lib.nixosSystem {
+      system = "x86_64-linux";
+      modules = [ ./configuration.nix ];
+    };
+
+    deploy.nodes.vault = {
+      hostname = "192.168.1.210";
+      fastConnection = true;
+      profiles = {
+        system = {
+          sshUser = "root";
+          path = deploy-rs.lib.x86_64-linux.activate.nixos self.nixosConfiguration.vault;
+          user = "root";
+        };
+      };
+    };
+  };
+}
