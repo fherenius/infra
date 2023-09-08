785f9b8c7d212d4b849935423c6cfa31e9df3283
Fester Herenius <me@festerherenius.nl>
2023-09-09 00:20:14 +0200

    Fixed Vault

--- lxc/vault/configuration.nix
+++ lxc/vault/configuration.nix
@@ -8,7 +8,31 @@
   # Enable Flakes and the new CLI
   nix.settings.experimental-features = [ "nix-command" "flakes" ];
 
+  # Environment seggints
   environment.systemPackages = with pkgs; [
     git # Used by Flake
   ];
+  time.timeZone = "Europe/Amsterdam";
+
+  # Add version of Nix that the host is running
+  system.stateVersion = "23.05";
+
+  # Services setup
+  services.openssh = {
+    enable = true;
+    settings = {
+      X11Forwarding = false;
+      PasswordAuthentication = false;
+    };
+  };
+
+  # Config the Vault user
+  users.users.vault = {
+    createHome = true;
+    home = "/home/vault";
+    isNormalUser = true;
+    openssh.authorizedKeys.keys = [
+      "sk-ssh-ed25519@openssh.com AAAAGnNrLXNzaC1lZDI1NTE5QG9wZW5zc2guY29tAAAAILRsXI5a6WLD6eflNESoq41hjyrsn6ySCe6qKD2m1CvyAAAABHNzaDo= fester@maccie.lan"
+    ];
+  };
 }
--- lxc/vault/home/home.nix
+++ lxc/vault/home/home.nix
@@ -1,8 +1,8 @@
-{ config, pkgs, ... }:
+{ lib, config, pkgs, ... }:
 
 {
   home.username = "vault";
-  home.homeDirectory = "/home/vault";
+  home.homeDirectory = lib.mkForce "/home/vault";
 
   home.packages = with pkgs; [
     vault-bin
@@ -10,4 +10,34 @@
 
   home.stateVersion = "23.05";
   programs.home-manager.enable = true;
+
+  # Included files
+  home.file.".config/vault/config.hcl" = {
+    enable = true;
+    executable = false;
+    source = "./vault-config.hcl";
+  };
+
+  # Programs to run at startup
+  systemd.user.services = {
+    vault = {
+      Unit = {
+        Description = "Runs a Vault instance";
+      };
+      Service = {
+        ExecStart = "/etc/profiles/per-user/vault/bin/vault server -config /home/vault/.config/vault/config.hcl";
+        ExecReload = "/bin/kill -HUP $MAINPID";
+        WorkingDirectory = "~";
+        Restart = "always";
+        KillSignal = "SIGTERM";
+        User = "vault";
+        Group = "vault";
+      };
+    };
+  };
+
+  # Environment variables for user
+  systemd.user.sessionVariables = {
+    VAULT_ENABLE_FILE_PERMISSIONS_CHECK = true;
+  };
 }
